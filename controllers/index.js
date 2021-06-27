0d6f87b7ac038c995c935129c4d605cd36981581
faithdinno <adebayooluwadara@gmail.com>
2021-06-27 14:58:10 +0100

    updates

--- controllers/index.js
+++ controllers/index.js
@@ -150,3 +150,40 @@ exports.createAnalysis = async(req, res) => {
         }
     }
 };
+
+
+/**
+ * 
+ * Get Analysis
+ */
+exports.getAnalysis = async(req, res) => {
+    try {
+        const QUERY = `SELECT * FROM ${SCHEMA}.${TABLE} WHERE shortedLink="${req.params.id}"`;
+        db.query(QUERY)
+            .then(result => {
+                console.log(result.data);
+                if (result.data != '') {
+                    res.status(200).json({
+                        message: "Fetched Successful",
+                        response: result
+                    });
+                } else {
+                    res.status(200).json({
+                        message: "No record found for the id given",
+                        response: result
+                    });
+                }
+            })
+            .catch(error => {
+                res.status(422).json({
+                    message: "An error occur",
+                    response: error
+                });
+            })
+    } catch (error) {
+        res.status(422).json({
+            message: "An error occur",
+            response: error
+        });
+    }
+};
